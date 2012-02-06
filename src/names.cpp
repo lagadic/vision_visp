5d65fb33cc31d6a552902b53e74074bdf4ffed03
filip novotny <filipnovo@gmail.com>
2012-02-06 15:38:25 +0100

    initial commit

---
+++ src/names.cpp
@@ -0,0 +1,61 @@
+/****************************************************************************
+ *
+ * $Id: file.h 3496 2011-11-22 15:14:32Z fnovotny $
+ *
+ * This file is part of the ViSP software.
+ * Copyright (C) 2005 - 2011 by INRIA. All rights reserved.
+ *
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * ("GPL") version 2 as published by the Free Software Foundation.
+ * See the file LICENSE.txt at the root directory of this source
+ * distribution for additional information about the GNU GPL.
+ *
+ * For using ViSP with software that can not be combined with the GNU
+ * GPL, please contact INRIA about acquiring a ViSP Professional
+ * Edition License.
+ *
+ * See http://www.irisa.fr/lagadic/visp/visp.html for more information.
+ *
+ * This software was developed at:
+ * INRIA Rennes - Bretagne Atlantique
+ * Campus Universitaire de Beaulieu
+ * 35042 Rennes Cedex
+ * France
+ * http://www.irisa.fr/lagadic
+ *
+ * If you have questions regarding the use of this file, please contact
+ * INRIA at visp@inria.fr
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Contact visp@irisa.fr if any conditions of this licensing are
+ * not clear to you.
+ *
+ * Description:
+ * File containing names of topics or services used all accross the package
+ *
+ * Authors:
+ * Filip Novotny
+ *
+ *
+ *****************************************************************************/
+
+/*!
+  \file names.cpp
+  \brief File containing names of topics or services used all accross the package
+*/
+
+#include "names.h"
+
+namespace visp_hand2eye_calibration
+{
+  std::string camera_object_topic("camera_object");
+  std::string world_effector_topic("world_effector");
+  std::string compute_effector_camera_service("compute_effector_camera");
+  std::string compute_effector_camera_quick_service("compute_effector_camera_quick");  
+  std::string reset_service("reset");
+} 
+
+
