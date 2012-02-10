def0c738547a63e6ede376dadc2a4717462f1076
Filip Novotny <filip.novotny@inria.fr>
2012-02-10 11:19:32 +0100

    replaces loop with memcpy in image conversion

--- src/conversions/image.cpp
+++ src/conversions/image.cpp
@@ -63,21 +63,13 @@ namespace visp_bridge{
     dst.encoding = sensor_msgs::image_encodings::MONO8;
     dst.step = src.getWidth();
     dst.data.resize(dst.height * dst.step);
-    for(unsigned i = 0; i < src.getWidth (); ++i)
-      for(unsigned j = 0; j < src.getHeight (); ++j)
-        dst.data[j * dst.step + i] = src[j][i];
+    memcpy(&dst.data[0],src.bitmap,dst.height * dst.step*sizeof(unsigned char));
 
     return dst;
   }
 
   vpImage<unsigned char> toVispImage(const sensor_msgs::Image& src){
-    using sensor_msgs::image_encodings::RGB8;
-    using sensor_msgs::image_encodings::RGBA8;
-    using sensor_msgs::image_encodings::BGR8;
-    using sensor_msgs::image_encodings::BGRA8;
     using sensor_msgs::image_encodings::MONO8;
-    using sensor_msgs::image_encodings::MONO16;
-    using sensor_msgs::image_encodings::numChannels;
 
     vpImage<unsigned char> dst(src.height,src.width);
 
