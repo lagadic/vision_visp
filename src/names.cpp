431865d146bba5262d7a98b879536d0ed46ca171
Fabien Spindler <Fabien>
2013-02-20 11:10:38 +0100

    Add klt pont topic

--- src/names.cpp
+++ src/names.cpp
@@ -5,6 +5,8 @@ namespace visp_auto_tracker
   std::string camera_info_topic("camera_info");
   std::string image_topic("image_raw");
   std::string moving_edge_sites_topic("/moving_edge_sites");
+  std::string klt_points_topic("/klt_points");
+
   std::string status_topic("status");
 
   std::string object_position_topic("object_position");
