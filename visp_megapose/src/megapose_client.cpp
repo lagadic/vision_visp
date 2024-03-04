8d6ccc6f97a7927ca7fc98522d91601ee023ad2a
m11112089 <kevin6231123@gmail.com>
2024-03-04 14:39:16 +0800

    ADD OeenCV define guard

--- visp_megapose/src/megapose_client.cpp
+++ visp_megapose/src/megapose_client.cpp
@@ -1,17 +1,17 @@
+#include <filesystem> // C++17 filesystem library for checking file existence
 #include <rclcpp/rclcpp.hpp>
-#include <filesystem>  // C++17 filesystem library for checking file existence
 
 // visp includes
 #include <visp3/core/vpTime.h>
-#include <visp3/gui/vpDisplayX.h>
 #include <visp3/detection/vpDetectorDNNOpenCV.h>
+#include <visp3/gui/vpDisplayX.h>
 
 // OpenCV/visp bridge includes
+#include <cv_bridge/cv_bridge.hpp>
+#include <opencv4/opencv2/opencv.hpp>
 #include <visp_bridge/3dpose.h>
 #include <visp_bridge/camera.h>
 #include <visp_bridge/image.h>
-#include <opencv4/opencv2/opencv.hpp>
-#include <cv_bridge/cv_bridge.hpp>
 
 // ROS2 includes
 #include "tf2_ros/transform_broadcaster.h"
@@ -27,22 +27,14 @@
 
 // ROS2 visp_megapose Server includes
 #include <visp_megapose/srv/init.hpp>
-#include <visp_megapose/srv/track.hpp>
 #include <visp_megapose/srv/render.hpp>
+#include <visp_megapose/srv/track.hpp>
 
 using namespace std::chrono_literals; // For using time literals like 1s
 
-enum DetectionMethod
-{
-  UNKNOWN,
-  CLICK,
-  DNN
-};
+enum DetectionMethod { UNKNOWN, CLICK, DNN };
 
-std::map<std::string, DetectionMethod> stringToDetectionMethod = {
-    {"UNKNOWN", UNKNOWN},
-    {"CLICK", CLICK},
-    {"DNN", DNN}};
+std::map<std::string, DetectionMethod> stringToDetectionMethod = {{"UNKNOWN", UNKNOWN}, {"CLICK", CLICK}, {"DNN", DNN}};
 
 bool fileExists(const std::string &path)
 {
@@ -81,7 +73,8 @@ private:
   void render_service_response_callback(rclcpp::Client<visp_megapose::srv::Render>::SharedFuture future);
   bool render_request_done_;
 
-  void broadcastTransform(const geometry_msgs::msg::Transform &transform, const std::string &child_frame_id, const std::string &camera_tf);
+  void broadcastTransform(const geometry_msgs::msg::Transform &transform, const std::string &child_frame_id,
+                          const std::string &camera_tf);
   geometry_msgs::msg::Transform transform_;
 
   vpColor interpolate(const vpColor &low, const vpColor &high, const float f);
@@ -119,8 +112,7 @@ MegaPoseClient::~MegaPoseClient()
 
 DetectionMethod MegaPoseClient::getDetectionMethodFromString(const std::string &str)
 {
-  if (stringToDetectionMethod.find(str) != stringToDetectionMethod.end())
-  {
+  if (stringToDetectionMethod.find(str) != stringToDetectionMethod.end()) {
     return stringToDetectionMethod[str];
   }
   return UNKNOWN; // Default case if string is not found
@@ -130,10 +122,8 @@ void MegaPoseClient::waitForImage()
 {
   rclcpp::Rate loop_rate(10);
   RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Waiting for a rectified image...");
-  while (rclcpp::ok())
-  {
-    if (got_image_)
-    {
+  while (rclcpp::ok()) {
+    if (got_image_) {
       RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Got image!");
       return;
     }
@@ -153,7 +143,8 @@ void MegaPoseClient::frameCallback(const sensor_msgs::msg::Image::ConstSharedPtr
 
   got_image_ = true;
 }
-void MegaPoseClient::broadcastTransform(const geometry_msgs::msg::Transform &transform, const std::string &child_frame_id, const std::string &camera_tf)
+void MegaPoseClient::broadcastTransform(const geometry_msgs::msg::Transform &transform,
+                                        const std::string &child_frame_id, const std::string &camera_tf)
 {
   static auto tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
   static geometry_msgs::msg::TransformStamped transformStamped;
@@ -167,28 +158,25 @@ void MegaPoseClient::spin()
 {
   // Get parameters
   std::string image_topic = this->declare_parameter<std::string>("image_topic", "/camera/camera/color/image_raw");
-  std::string camera_info_topic = this->declare_parameter<std::string>("camera_info_topic", "/camera/camera/color/camera_info");
+  std::string camera_info_topic =
+      this->declare_parameter<std::string>("camera_info_topic", "/camera/camera/color/camera_info");
   std::string camera_tf = this->declare_parameter<std::string>("camera_tf", "camera_color_optical_frame");
   RCLCPP_INFO(this->get_logger(), "Subscribing to image topic: %s", image_topic.c_str());
   RCLCPP_INFO(this->get_logger(), "Subscribing to camera info topic: %s", camera_info_topic.c_str());
 
   std::string detectorMethod = this->declare_parameter<std::string>("detector_method", "DNN");
   RCLCPP_INFO(this->get_logger(), "Detection method: %s", detectorMethod.c_str());
-  if (getDetectionMethodFromString(detectorMethod) == UNKNOWN)
-  {
+  if (getDetectionMethodFromString(detectorMethod) == UNKNOWN) {
     RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Unknown detection method. Exiting.");
     rclcpp::shutdown();
   }
 
   std::string detectorModelPath = this->declare_parameter<std::string>("detector_model_path", "none");
-  if (!fileExists(detectorModelPath))
-  {
+  if (!fileExists(detectorModelPath)) {
     RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Detector model path does not exist. Exiting.");
     rclcpp::shutdown();
   }
 
-  std::string detectorConfig = "none";
-  std::string detectorFramework = "onnx", detectorTypeString = "yolov7";
   std::string objectName = this->declare_parameter<std::string>("object_name", "cube");
   RCLCPP_INFO(this->get_logger(), "Object name: %s", objectName.c_str());
   std::vector<std::string> labels = {objectName};
@@ -201,19 +189,24 @@ void MegaPoseClient::spin()
       std::bind(&MegaPoseClient::frameCallback, this, std::placeholders::_1, std::placeholders::_2));
   waitForImage();
 
-  // Initialize DNN detector if detectorMethod is DNN
-  if (getDetectionMethodFromString(detectorMethod) == DNN)
-  {
+  std::string detectorConfig = "none";
+  std::string detectorFramework = "onnx", detectorTypeString = "yolov7";
+#if (VISP_HAVE_OPENCV_VERSION >= 0x030403) && defined(HAVE_OPENCV_DNN) &&                                              \
+    ((__cplusplus >= 201703L) || (defined(_MSVC_LANG) && (_MSVC_LANG >= 201703L)))
+  if (getDetectionMethodFromString(detectorMethod) == DNN) {
     float detectorMeanR = 0.f, detectorMeanG = 0.f, detectorMeanB = 0.f;
     float detectorConfidenceThreshold = 0.65f, detectorNmsThreshold = 0.5f, detectorFilterThreshold = -0.25f;
     float detectorScaleFactor = 0.0039f;
     bool detectorSwapRB = false;
 
-    vpDetectorDNNOpenCV::DNNResultsParsingType detectorType = vpDetectorDNNOpenCV::dnnResultsParsingTypeFromString(detectorTypeString);
-    vpDetectorDNNOpenCV::NetConfig netConfig(detectorConfidenceThreshold, detectorNmsThreshold, labels, cv::Size(640, 640), detectorFilterThreshold);
+    vpDetectorDNNOpenCV::DNNResultsParsingType detectorType =
+        vpDetectorDNNOpenCV::dnnResultsParsingTypeFromString(detectorTypeString);
+    vpDetectorDNNOpenCV::NetConfig netConfig(detectorConfidenceThreshold, detectorNmsThreshold, labels,
+                                             cv::Size(640, 640), detectorFilterThreshold);
     // vpDetectorDNNOpenCV dnn(netConfig, detectorType);  // I don't know why this doesn't work. If I use this
-    // it will cause the error "Cuda and/or GPU driver might not be correctly installed. Setting preferable backend to CPU and trying again.
-    // terminate called after throwing an instance of 'cv::Exception"
+    // it will cause the error "Cuda and/or GPU driver might not be correctly installed. Setting preferable backend to
+    // CPU and trying again. terminate called after throwing an instance of 'cv::Exception"
+    vpDetectorDNNOpenCV dnn_;
     dnn_.setNetConfig(netConfig);
     dnn_.setParsingMethod(detectorType);
     dnn_.readNet(detectorModelPath, detectorConfig, detectorFramework);
@@ -221,9 +214,9 @@ void MegaPoseClient::spin()
     dnn_.setScaleFactor(detectorScaleFactor);
     dnn_.setSwapRB(detectorSwapRB);
   }
+#endif
 
   vpDisplayX *d = NULL;
-
   d = new vpDisplayX();
   rclcpp::spin_some(this->get_node_base_interface());
   d->init(vpI_); // also init display
@@ -235,12 +228,9 @@ void MegaPoseClient::spin()
 
   auto render_client = this->create_client<visp_megapose::srv::Render>("render_object");
 
-  while (!initial_pose_client->wait_for_service(1s) &&
-         !track_pose_client->wait_for_service(1s) &&
-         !render_client->wait_for_service(1s))
-  {
-    if (!rclcpp::ok())
-    {
+  while (!initial_pose_client->wait_for_service(1s) && !track_pose_client->wait_for_service(1s) &&
+         !render_client->wait_for_service(1s)) {
+    if (!rclcpp::ok()) {
       RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
       return;
     }
@@ -248,27 +238,23 @@ void MegaPoseClient::spin()
   }
 
   rclcpp::Rate loop_rate(10);
-  while (rclcpp::ok())
-  {
+  while (rclcpp::ok()) {
     vpDisplay::display(vpI_);
     rclcpp::spin_some(this->get_node_base_interface());
 
-    if (!initialized_)
-    {
+    if (!initialized_) {
       std::optional<vpRect> detection = std::nullopt;
-      if (getDetectionMethodFromString(detectorMethod) == DNN)
-      {
+#if (VISP_HAVE_OPENCV_VERSION >= 0x030403) && defined(HAVE_OPENCV_DNN) &&                                              \
+    ((__cplusplus >= 201703L) || (defined(_MSVC_LANG) && (_MSVC_LANG >= 201703L)))
+      if (getDetectionMethodFromString(detectorMethod) == DNN) {
         detection = detectObjectForInitMegaposeDnn(objectName);
-        printf("DNN");
       }
-
-      else if (getDetectionMethodFromString(detectorMethod) == CLICK)
-      {
+#endif
+      if (getDetectionMethodFromString(detectorMethod) == CLICK) {
         detection = detectObjectForInitMegaposeClick();
       }
 
-      if (detection && init_request_done_)
-      {
+      if (detection && init_request_done_) {
         static auto initial_pose_request = std::make_shared<visp_megapose::srv::Init::Request>();
         initial_pose_request->object_name = objectName;
         initial_pose_request->topleft_i = detection->getTopLeft().get_i();
@@ -279,16 +265,13 @@ void MegaPoseClient::spin()
         initial_pose_request->camera_info = *roscam_info_;
 
         auto result_future = initial_pose_client->async_send_request(
-            initial_pose_request, std::bind(&MegaPoseClient::initial_pose_service_response_callback, this,
-                                            std::placeholders::_1));
+            initial_pose_request,
+            std::bind(&MegaPoseClient::initial_pose_service_response_callback, this, std::placeholders::_1));
         init_request_done_ = false;
       }
-    }
-    else if (initialized_)
-    {
+    } else if (initialized_) {
       static auto track_pose_request = std::make_shared<visp_megapose::srv::Track::Request>();
-      if (track_request_done_)
-      {
+      if (track_request_done_) {
         track_pose_request->object_name = objectName;
         track_pose_request->init_pose = transform_;
         track_pose_request->refiner_iterations = 1;
@@ -296,13 +279,12 @@ void MegaPoseClient::spin()
         track_pose_request->camera_info = *roscam_info_;
 
         auto result_future = track_pose_client->async_send_request(
-            track_pose_request, std::bind(&MegaPoseClient::track_pose_service_response_callback, this,
-                                          std::placeholders::_1));
+            track_pose_request,
+            std::bind(&MegaPoseClient::track_pose_service_response_callback, this, std::placeholders::_1));
         track_request_done_ = false;
       }
       static auto render_request = std::make_shared<visp_megapose::srv::Render::Request>();
-      if (render_request_done_ && overlayModel_)
-      {
+      if (render_request_done_ && overlayModel_) {
         render_request->object_name = objectName;
         render_request->pose = transform_;
         render_request->camera_info = *roscam_info_;
@@ -313,8 +295,7 @@ void MegaPoseClient::spin()
 
       std::string keyboardEvent;
       const bool keyPressed = vpDisplay::getKeyboardEvent(vpI_, keyboardEvent, false);
-      if (keyPressed)
-      {
+      if (keyPressed) {
         if (keyboardEvent == "t")
           overlayModel_ = !overlayModel_;
       }
@@ -331,10 +312,8 @@ void MegaPoseClient::spin()
 
     vpDisplay::flush(vpI_);
     vpMouseButton::vpMouseButtonType button;
-    if (vpDisplay::getClick(vpI_, button, false))
-    {
-      if (button == vpMouseButton::button3)
-      {
+    if (vpDisplay::getClick(vpI_, button, false)) {
+      if (button == vpMouseButton::button3) {
         break; // Right click to stop
       }
     }
@@ -351,37 +330,27 @@ std::optional<vpRect> MegaPoseClient::detectObjectForInitMegaposeDnn(const std::
   std::vector<vpDetectorDNNOpenCV::DetectedFeatures2D> detections_vec;
   dnn_.detect(I, detections_vec);
   std::vector<vpDetectorDNNOpenCV::DetectedFeatures2D> matchingDetections;
-  for (const auto &detection : detections_vec)
-  {
+  for (const auto &detection : detections_vec) {
     std::optional<std::string> classnameOpt = detection.getClassName();
-    if (classnameOpt)
-    {
-      if (*classnameOpt == detectionLabel)
-      {
+    if (classnameOpt) {
+      if (*classnameOpt == detectionLabel) {
         matchingDetections.push_back(detection);
       }
     }
   }
-  if (matchingDetections.size() == 0)
-  {
+  if (matchingDetections.size() == 0) {
     printf("No matching detections\n");
     return std::nullopt;
-  }
-  else if (matchingDetections.size() == 1)
-  {
+  } else if (matchingDetections.size() == 1) {
     printf("One matching detection\n");
     return matchingDetections[0].getBoundingBox();
-  }
-  else
-  {
+  } else {
     printf("Multiple matching detections\n");
     vpRect best;
     double highestConf = 0.0;
-    for (const auto &detection : matchingDetections)
-    {
+    for (const auto &detection : matchingDetections) {
       const double conf = detection.getConfidenceScore();
-      if (conf > highestConf)
-      {
+      if (conf > highestConf) {
         highestConf = conf;
         best = detection.getBoundingBox();
       }
@@ -391,28 +360,26 @@ std::optional<vpRect> MegaPoseClient::detectObjectForInitMegaposeDnn(const std::
 
   return std::nullopt;
 }
-void MegaPoseClient::initial_pose_service_response_callback(rclcpp::Client<visp_megapose::srv::Init>::SharedFuture future)
+void MegaPoseClient::initial_pose_service_response_callback(
+    rclcpp::Client<visp_megapose::srv::Init>::SharedFuture future)
 {
   init_request_done_ = true;
   transform_ = future.get()->pose;
   confidence_ = future.get()->confidence;
-  if (confidence_ < reinitThreshold_)
-  {
+  if (confidence_ < reinitThreshold_) {
     RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Initial pose not reliable, reinitializing...");
-  }
-  else
-  {
+  } else {
     initialized_ = true;
     RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Initialized successfully!");
   }
 }
-void MegaPoseClient::track_pose_service_response_callback(rclcpp::Client<visp_megapose::srv::Track>::SharedFuture future)
+void MegaPoseClient::track_pose_service_response_callback(
+    rclcpp::Client<visp_megapose::srv::Track>::SharedFuture future)
 {
   track_request_done_ = true;
   transform_ = future.get()->pose;
   confidence_ = future.get()->confidence;
-  if (confidence_ < reinitThreshold_)
-  {
+  if (confidence_ < reinitThreshold_) {
     initialized_ = false;
     RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Tracking lost, reinitializing...");
   }
@@ -450,8 +417,7 @@ std::optional<vpRect> MegaPoseClient::detectObjectForInitMegaposeClick()
 
   const vpImagePoint textPosition(10.0, 20.0);
 
-  if (startLabelling)
-  {
+  if (startLabelling) {
     vpImagePoint topLeft, bottomRight;
     vpDisplay::displayText(vpI_, textPosition, "Click the upper left corner of the bounding box", vpColor::red);
     vpDisplay::flush(vpI_);
@@ -463,9 +429,7 @@ std::optional<vpRect> MegaPoseClient::detectObjectForInitMegaposeClick()
     vpDisplay::getClick(vpI_, bottomRight, true);
     vpRect bb(topLeft, bottomRight);
     return bb;
-  }
-  else
-  {
+  } else {
     vpDisplay::display(vpI_);
     vpDisplay::displayText(vpI_, textPosition,
                            "Click when the object is visible and static to start reinitializing megapose.",
@@ -477,12 +441,9 @@ std::optional<vpRect> MegaPoseClient::detectObjectForInitMegaposeClick()
 void MegaPoseClient::overlayRender(const vpImage<vpRGBa> &overlay)
 {
   const vpRGBa black = vpRGBa(0, 0, 0);
-  for (unsigned int i = 0; i < height_; ++i)
-  {
-    for (unsigned int j = 0; j < width_; ++j)
-    {
-      if (overlay[i][j] != black)
-      {
+  for (unsigned int i = 0; i < height_; ++i) {
+    for (unsigned int j = 0; j < width_; ++j) {
+      if (overlay[i][j] != black) {
         vpI_[i][j] = overlay[i][j];
       }
     }
